8c7583898b158a935470dd462c06f23318ad0a3f
Nate Sweet <nathanjsweet@gmail.com>
2016-02-16 21:26:17 -0600

    fixing waitgroup race

--- throttlelock.go
+++ throttlelock.go
@@ -3,39 +3,60 @@ package throttlelock
 import "sync"
 
 type ThrottleLock struct {
-	C *sync.Cond
-	W *sync.WaitGroup
-	Cap,
-	Cur int
+	turnCondition,
+	awaitAllCondition *sync.Cond
+	waitGroup *sync.WaitGroup
+	runCapacity,
+	currentRunning,
+	totalExpectedWaiters,
+	currentWaiters uint64
+	awaitAllIsSleeping bool
 }
 
-func NewThrottleLock(capacity int) *ThrottleLock {
+func NewThrottleLock(runCapacity, totalExpectedWaiters uint64) *ThrottleLock {
 	tl := new(ThrottleLock)
-	tl.C = sync.NewCond(&sync.Mutex{})
-	tl.W = &sync.WaitGroup{}
-	tl.Cap = capacity
-	tl.Cur = 0
+	tl.turnCondition = sync.NewCond(&sync.Mutex{})
+	tl.awaitAllCondition = sync.NewCond(&sync.Mutex{})
+	tl.waitGroup = &sync.WaitGroup{}
+	tl.runCapacity = runCapacity
+	tl.currentRunning = uint64(0)
+	tl.totalExpectedWaiters = totalExpectedWaiters
+	tl.currentWaiters = uint64(0)
+	tl.awaitAllIsSleeping = false
 	return tl
 }
 
 func (tl *ThrottleLock) AwaitAll() {
-	tl.W.Wait()
+	tl.awaitAllCondition.L.Lock()
+	if tl.currentWaiters == tl.totalExpectedWaiters {
+		tl.awaitAllCondition.L.Unlock()
+	} else {
+		tl.awaitAllIsSleeping = true
+		tl.awaitAllCondition.Wait()
+	}
+	tl.waitGroup.Wait()
 }
 
 func (tl *ThrottleLock) WaitForTurn() {
-	tl.W.Add(1)
-	tl.C.L.Lock()
-	if tl.Cur == tl.Cap {
-		tl.C.Wait()
+	tl.waitGroup.Add(1)
+	tl.awaitAllCondition.L.Lock()
+	tl.currentWaiters++
+	if tl.currentWaiters == tl.totalExpectedWaiters && tl.awaitAllIsSleeping {
+		tl.awaitAllCondition.Signal()
 	}
-	tl.Cur++
-	tl.C.L.Unlock()
+	tl.awaitAllCondition.L.Unlock()
+	tl.turnCondition.L.Lock()
+	if tl.currentRunning == tl.runCapacity {
+		tl.turnCondition.Wait()
+	}
+	tl.currentRunning++
+	tl.turnCondition.L.Unlock()
 }
 
 func (tl *ThrottleLock) Done() {
-	tl.C.L.Lock()
-	tl.Cur--
-	tl.C.L.Unlock()
-	tl.C.Signal()
-	tl.W.Done()
+	tl.turnCondition.L.Lock()
+	tl.currentRunning--
+	tl.turnCondition.L.Unlock()
+	tl.turnCondition.Signal()
+	tl.waitGroup.Done()
 }
--- throttlelock_test.go
+++ throttlelock_test.go
@@ -1,31 +1,44 @@
 package throttlelock
 
 import (
+	"sync/atomic"
 	"testing"
 	"time"
 )
 
 func TestThrottleLock(t *testing.T) {
 	tl := NewThrottleLock(10, 50)
+	v := int64(0)
 	for i := 0; i < 50; i++ {
 		go func(t1 int) {
 			tl.WaitForTurn()
 			defer tl.Done()
+			atomic.AddInt64(&v, int64(1))
 		}(i)
 	}
 	tl.AwaitAll()
+	if v != 50 {
+		t.Errorf("Not all go routines ran. Value is %d", v)
+	}
 }
 
 func TestThrottleLockWithDelayedWait(t *testing.T) {
 	tl := NewThrottleLock(10, 50)
+	v := int64(0)
 	for i := 0; i < 50; i++ {
 		go func(t1 int) {
 			if t1 == 0 {
-				time.Sleep(time.Second * 10)
+				// two seconds is more than enough time for 50
+				// iterations to complete
+				time.Sleep(time.Second * 2)
 			}
 			tl.WaitForTurn()
 			defer tl.Done()
+			atomic.AddInt64(&v, int64(1))
 		}(i)
 	}
 	tl.AwaitAll()
+	if v != 50 {
+		t.Errorf("Not all go routines ran. Value is %d", v)
+	}
 }
