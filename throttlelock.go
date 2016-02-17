7ec3e4df57b38709be553640f6465bddad375d29
Nate Sweet <nathanjsweet@gmail.com>
2016-02-16 22:01:35 -0600

    let's make this simpler

--- throttlelock.go
+++ throttlelock.go
@@ -3,48 +3,27 @@ package throttlelock
 import "sync"
 
 type ThrottleLock struct {
-	turnCondition,
-	awaitAllCondition *sync.Cond
-	waitGroup *sync.WaitGroup
+	turnCondition *sync.Cond
+	waitGroup     *sync.WaitGroup
 	runCapacity,
-	currentRunning,
-	totalExpectedWaiters,
-	currentWaiters uint64
-	awaitAllIsSleeping bool
+	currentRunning int
 }
 
-func NewThrottleLock(runCapacity, totalExpectedWaiters uint64) *ThrottleLock {
+func NewThrottleLock(runCapacity, totalExpectedWaiters int) *ThrottleLock {
 	tl := new(ThrottleLock)
 	tl.turnCondition = sync.NewCond(&sync.Mutex{})
-	tl.awaitAllCondition = sync.NewCond(&sync.Mutex{})
 	tl.waitGroup = &sync.WaitGroup{}
 	tl.runCapacity = runCapacity
-	tl.currentRunning = uint64(0)
-	tl.totalExpectedWaiters = totalExpectedWaiters
-	tl.currentWaiters = uint64(0)
-	tl.awaitAllIsSleeping = false
+	tl.currentRunning = 0
+	tl.waitGroup.Add(totalExpectedWaiters)
 	return tl
 }
 
 func (tl *ThrottleLock) AwaitAll() {
-	tl.awaitAllCondition.L.Lock()
-	if tl.currentWaiters == tl.totalExpectedWaiters {
-		tl.awaitAllCondition.L.Unlock()
-	} else {
-		tl.awaitAllIsSleeping = true
-		tl.awaitAllCondition.Wait()
-	}
 	tl.waitGroup.Wait()
 }
 
 func (tl *ThrottleLock) WaitForTurn() {
-	tl.waitGroup.Add(1)
-	tl.awaitAllCondition.L.Lock()
-	tl.currentWaiters++
-	if tl.currentWaiters == tl.totalExpectedWaiters && tl.awaitAllIsSleeping {
-		tl.awaitAllCondition.Signal()
-	}
-	tl.awaitAllCondition.L.Unlock()
 	tl.turnCondition.L.Lock()
 	if tl.currentRunning == tl.runCapacity {
 		tl.turnCondition.Wait()
